9a1ae11861048e2c9605178ddacdaae1d5a88aa8
vishalarora <vishal.arora@ixigo.com>
2019-06-15 22:13:00 +0530

    added reenter shared element transition

--- app/src/main/java/com/effort/images/ui/ImageViewerActivity.java
+++ app/src/main/java/com/effort/images/ui/ImageViewerActivity.java
@@ -1,10 +1,21 @@
 package com.effort.images.ui;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.SharedElementCallback;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -15,40 +26,159 @@ import com.bumptech.glide.request.target.Target;
 import com.effort.images.R;
 import com.effort.images.data.ImageResource;
 
+import java.util.List;
+import java.util.Map;
+
 public class ImageViewerActivity extends AppCompatActivity {
 
-    public static final String KEY_IMAGE_RESOURCE = "KEY_IMAGE_RESOURCE";
+    public static final String KEY_IMAGE_RESOURCES = "KEY_IMAGE_RESOURCES";
+    public static final String KEY_ITEM_INDEX = "KEY_ITEM_INDEX";
+    public static final String KEY_CURRENT_SAVED_POSITION = "KEY_CURRENT_SAVED_POSITION";
+
+    private boolean isReturning;
+    private int startingPosition = 0;
+    private int currentPosition = 0;
+    private FullImageAdapter imagePagerAdapter;
+
+    private final SharedElementCallback sharedElementCallback = new SharedElementCallback() {
+        @Override
+        public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
+            super.onMapSharedElements(names, sharedElements);
+            if (isReturning) {
+                final View sharedElement = imagePagerAdapter.getView(currentPosition);
+
+                if (startingPosition != currentPosition) {
+                    names.clear();
+                    names.add(ViewCompat.getTransitionName(sharedElement));
+
+                    sharedElements.clear();
+                    sharedElements.put(ViewCompat.getTransitionName(sharedElement), sharedElement);
+                }
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_viewer);
-        supportPostponeEnterTransition();
+        ActivityCompat.postponeEnterTransition(this);
+        ActivityCompat.setEnterSharedElementCallback(this, sharedElementCallback);
 
-        ImageResource imageResource = (ImageResource) getIntent().getSerializableExtra(KEY_IMAGE_RESOURCE);
-        ImageView imageView = findViewById(R.id.iv_full_image);
-        ViewCompat.setTransitionName(imageView, "" + imageResource.getId());
+        final int index = getIntent().getIntExtra(KEY_ITEM_INDEX, 0);
+        final int startingPosition = index > 0 ? index : 0;
+        final List<ImageResource> imageResources = (List<ImageResource>) getIntent().getSerializableExtra(KEY_IMAGE_RESOURCES);
 
-        Glide.with(this)
-                .load(imageResource.getHdUrl())
-                .listener(new RequestListener<Drawable>() {
-                    @Override
-                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
-                        return false;
-                    }
+        currentPosition = savedInstanceState == null ? startingPosition : savedInstanceState.getInt(KEY_CURRENT_SAVED_POSITION);
 
-                    @Override
-                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
-                        supportStartPostponedEnterTransition();
-                        return false;
-                    }
-                }).into(imageView);
+        final ViewPager viewPager = findViewById(R.id.image_pager);
+        imagePagerAdapter = new FullImageAdapter(imageResources, currentPosition);
 
+        viewPager.setAdapter(imagePagerAdapter);
+        viewPager.setCurrentItem(currentPosition);
+        viewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                currentPosition = position;
+            }
+        });
+    }
+
+    @Override
+    public void supportFinishAfterTransition() {
+        isReturning = true;
+        Intent data = new Intent();
+        data.putExtra(MainActivity.KEY_START_ITEM_POSITION, startingPosition);
+        data.putExtra(MainActivity.KEY_CURRENT_ITEM_POSITION, currentPosition);
+        setResult(Activity.RESULT_OK, data);
+        super.supportFinishAfterTransition();
     }
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
         supportFinishAfterTransition();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_CURRENT_SAVED_POSITION, currentPosition);
+    }
+
+    class FullImageAdapter extends PagerAdapter {
+
+        private final List<ImageResource> imageResourceList;
+        private final int currentPosition;
+        private final SparseArray<View> views;
+
+        public FullImageAdapter(List<ImageResource> imageResourceList, int currentPosition) {
+            this.imageResourceList = imageResourceList;
+            this.currentPosition = currentPosition;
+            this.views = new SparseArray<>(this.imageResourceList.size());
+        }
+
+        @NonNull
+        @Override
+        public Object instantiateItem(@NonNull ViewGroup container, int position) {
+            ImageResource imageResource = imageResourceList.get(position);
+            final ImageView imageView = new ImageView(container.getContext());
+            ViewCompat.setTransitionName(imageView, "" + imageResource.getId());
+            views.put(position, imageView);
+            if (position == currentPosition) {
+                Glide.with(container.getContext())
+                        .load(imageResource.getHdUrl())
+                        .listener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                startPostTransition(imageView);
+                                return false;
+                            }
+                        }).into(imageView);
+            } else {
+                Glide.with(container.getContext())
+                        .load(imageResource.getHdUrl())
+                        .into(imageView);
+            }
+            container.addView(imageView);
+            return imageView;
+        }
+
+        private void startPostTransition(final View sharedView) {
+            sharedView.getViewTreeObserver().addOnPreDrawListener(
+                    new ViewTreeObserver.OnPreDrawListener() {
+                        @Override
+                        public boolean onPreDraw() {
+                            sharedView.getViewTreeObserver().removeOnPreDrawListener(this);
+                            supportStartPostponedEnterTransition();
+                            return true;
+                        }
+                    });
+        }
+
+        @Override
+        public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+            views.remove(position);
+            container.removeView((View) object);
+        }
+
+        @Override
+        public int getCount() {
+            return imageResourceList.size();
+        }
+
+        @Override
+        public boolean isViewFromObject(@NonNull View view, @NonNull Object o) {
+            return view == o;
+        }
+
+        public View getView(int currentPosition) {
+            return views.get(currentPosition);
+        }
     }
 }
--- app/src/main/java/com/effort/images/ui/MainActivity.java
+++ app/src/main/java/com/effort/images/ui/MainActivity.java
@@ -2,7 +2,9 @@ package com.effort.images.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.app.SharedElementCallback;
 import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -13,15 +15,21 @@ import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.effort.images.ImageApplication;
 import com.effort.images.R;
+import com.effort.images.data.ImageResource;
 import com.effort.images.di.component.DaggerMainActivityComponent;
 import com.effort.images.di.component.MainActivityComponent;
 import com.effort.images.di.module.MainActivityModule;
 import com.effort.images.lifecycle.ImagesViewModel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -29,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
 
     // keys
     private static final String KEY_SEARCH_QUERY = "KEY_SEARCH_QUERY";
+    public static final String KEY_START_ITEM_POSITION = "KEY_START_ITEM_POSITION";
+    public static final String KEY_CURRENT_ITEM_POSITION = "KEY_CURRENT_ITEM_POSITION";
 
     // dependencies
     @Inject
@@ -43,11 +53,38 @@ public class MainActivity extends AppCompatActivity {
 
     private ImagesAdapter imagesAdapter;
     private GridLayoutManager layoutManager;
+    private Bundle reenterState;
+
+    private final SharedElementCallback exitElementCallback = new SharedElementCallback() {
+        @Override
+        public void onMapSharedElements(List<String> names, Map<String, View> sharedElements) {
+            super.onMapSharedElements(names, sharedElements);
+            if (reenterState != null) {
+                final int startingPosition = reenterState.getInt(KEY_START_ITEM_POSITION);
+                final int currentPosition = reenterState.getInt(KEY_CURRENT_ITEM_POSITION);
+
+                if (startingPosition != currentPosition) {
+                    final String newTransitionName = "" + imagesAdapter.getCurrentList().snapshot().get(currentPosition).getId();
+                    final View newSharedElement = rvImages.findViewHolderForAdapterPosition(currentPosition).itemView.findViewById(R.id.iv_image);
+                    if (newSharedElement != null) {
+                        names.clear();
+                        names.add(newTransitionName);
+
+                        sharedElements.clear();
+                        sharedElements.put(newTransitionName, newSharedElement);
+                    }
+                }
+                reenterState = null;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.setExitSharedElementCallback(this, exitElementCallback);
 
         mainActivityComponent = DaggerMainActivityComponent.builder()
                 .mainActivityModule(new MainActivityModule(this))
@@ -109,8 +146,10 @@ public class MainActivity extends AppCompatActivity {
 
         this.imagesAdapter.setItemClickListener((item, position, itemView) -> {
             final View imageView = rvImages.findViewHolderForAdapterPosition(position).itemView.findViewById(R.id.iv_image);
+            ArrayList<ImageResource> imageResources = new ArrayList<>(imagesAdapter.getCurrentList().snapshot());
             Intent intent = new Intent(this, ImageViewerActivity.class);
-            intent.putExtra(ImageViewerActivity.KEY_IMAGE_RESOURCE, item);
+            intent.putExtra(ImageViewerActivity.KEY_IMAGE_RESOURCES, imageResources);
+            intent.putExtra(ImageViewerActivity.KEY_ITEM_INDEX, position);
             ActivityOptionsCompat options = ActivityOptionsCompat.
                     makeSceneTransitionAnimation(this, imageView, ViewCompat.getTransitionName(imageView));
             startActivity(intent, options.toBundle());
@@ -129,6 +168,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void requestImages(String keyword) {
         imagesViewModel.requestImages(keyword);
+    }
+
+    @Override
+    public void onActivityReenter(int resultCode, Intent data) {
+        super.onActivityReenter(resultCode, data);
+        reenterState = data.getExtras();
+        final int startingPosition = data.getIntExtra(KEY_START_ITEM_POSITION, 0);
+        final int currentPosition = data.getIntExtra(KEY_CURRENT_ITEM_POSITION, 0);
+        if (startingPosition != currentPosition) rvImages.scrollToPosition(currentPosition);
+        ActivityCompat.postponeEnterTransition(this);
+
+        rvImages.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                rvImages.getViewTreeObserver().removeOnPreDrawListener(this);
+                ActivityCompat.startPostponedEnterTransition(MainActivity.this);
+                return true;
+            }
+        });
     }
 
     @Override
